926c8b403e24b6963033e2cf72b01f3baa892cac
jana.lyon <jana.lyon@icrossing.co.uk>
2017-03-16 09:46:11 +0000

    Respond.js and html5shiv added

--- wp-content/themes/james-reed-portfolio/functions.php
+++ wp-content/themes/james-reed-portfolio/functions.php
@@ -150,6 +150,18 @@ function james_reed_portfolio_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'james_reed_portfolio_scripts' );
 
+if( !function_exists('ie_scripts')) {
+    function ie_scripts(){
+        echo '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->';
+        echo '<!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->';
+        echo '<!--[if lt IE 9]>';
+        echo '<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>';
+        echo '<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>';
+        echo '<![endif]-->';
+    }
+    add_action('wp_head', 'ie_scripts');
+} //end if
+
 /**
  * Implement the Custom Header feature.
  */
